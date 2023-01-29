f8a744ab8fc4bc31ebdee3e467c38a5bd3fa0ead
arpacique <kemalemrearpaci@gmail.com>
2023-01-29 14:29:48 +0300

    Add missing commas to fix build error

--- cockpit/keymaps/arpacique/keymap.c
+++ cockpit/keymaps/arpacique/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,     GUI_T(KC_A), ALT_T(KC_S), SFT_T(KC_D), CTL_T(KC_F), KC_G,                    KC_H,  CTL_T(KC_J), SFT_T(KC_K), ALT_T(KC_L), GUI_T(KC_SCLN), KC_QUOT,
   KC_LSFT,    KC_Z,        KC_X,        KC_C,       KC_V,         KC_B,                    KC_N,  KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,        KC_SFTENT,  
                                                                           KC_MPLY,   KC_MUTE,
-                                          KC_LCTL,   LOWER,    LT(ARROW, KC_SPC),  LT(ARROW, KC_SPC),   RAISE,    KC_RGUI
+                                          KC_LCTL,   LOWER,    LT(ARROW, KC_SPC),  LT(ARROW, KC_SPC),   RAISE,    KC_RGUI,
                                                                                  KC_UP,
                                                                     KC_LEFT,    KC_DOWN,  KC_RGHT
 
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,            KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
     KC_CAPS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,           KC_F12,  _______, _______, KC_HOME, KC_END , _______,
                                                      KC_MPLY,   KC_MUTE,
-                             _______,   _______, _______,         _______,   _______,  _______
+                             _______,   _______, _______,         _______,   _______,  _______,
                                                            KC_UP,
                                               KC_LEFT,    KC_DOWN,  KC_RGHT
 
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
   KC_CAPS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,               KC_F12,  KC_LABK, KC_RABK, KC_PGUP, KC_PGDN, _______,
                                                      KC_MPLY,   KC_MUTE,
-                           _______,  _______,   _______,           _______,  _______,  _______
+                           _______,  _______,   _______,           _______,  _______,  _______,
                                                            KC_UP,
                                               KC_LEFT,    KC_DOWN,  KC_RGHT
   ),
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   CG_NORM,            CG_SWAP,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
                                                      KC_MPLY,   KC_MUTE,            
-                                     _______,   _______, XXXXXXX,        XXXXXXX,  _______,  _______
+                                     _______,   _______, XXXXXXX,        XXXXXXX,  _______,  _______,
                                                                   KC_UP,
                                                     KC_LEFT,    KC_DOWN,  KC_RGHT
   ),
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT,    KC_END,           _______, KC_4, KC_5, KC_6,  KC_ASTERISK, KC_EQUAL,
     _______, _______, _______, _______, C(KC_END),  KC_PGDN,          KC_0   , KC_1, KC_2, KC_3,  KC_MINUS,    KC_PLUS,
                                                          KC_MPLY,   KC_MUTE,                         
-                               _______, _______,   _______,           _______,  KC_0,  KC_DOT
+                               _______, _______,   _______,           _______,  KC_0,  KC_DOT,
                                                             KC_UP,
                                                 KC_LEFT,    KC_DOWN,  KC_RGHT
   )
